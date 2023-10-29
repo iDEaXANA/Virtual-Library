579860bfb0b883ce48024bc288d8680ff7cc5599
Bilal MB <bmbenkada@gmail.com>
2023-10-29 13:12:13 +0000

    Completed and passed all tests!

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,27 @@
 // import the Media class:
-
+const Media = require("./Media");
 // create your Movie class:
+class Movie extends Media {
+  constructor(title, year, genre, director, duration, rating) {
+    super(title, year, genre);
+    this.director = director;
+    this.duration = duration;
+    this.rating = rating;
+  }
 
+  summary() {
+    return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`;
+  }
+  static longestMovie(movies) {
+    if (movies.length === 0) return null;
+    let longestDuration = movies[0];
+    for (let i = 1; i < movies.length; i++) {
+      if (movies[i].duration > longestDuration.duration) {
+        longestDuration = movies[i];
+      }
+    }
+    return longestDuration;
+  }
+}
 // don't change below
 module.exports = Movie;
--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,27 @@
 // import the Media class:
-
+const Media = require("./Media");
 // create your Music class:
+class Music extends Media {
+  constructor(title, year, genre, artist, length) {
+    super(title, year, genre);
+    this.artist = artist;
+    this.length = length;
+  }
 
+  summary() {
+    return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`;
+  }
+  static shortestAlbum(songs) {
+    if (songs.length === 0) return null;
+    let shortestLength = songs[0];
+
+    for (let i = 1; i < songs.length; i++) {
+      if (songs[i].length < shortestLength.length) {
+        shortestLength = songs[i];
+      }
+    }
+    return shortestLength;
+  }
+}
 // don't change below
 module.exports = Music;
